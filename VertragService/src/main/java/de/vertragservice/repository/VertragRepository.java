7634fb4613aba2f9ebfc7e4701ce1f9b88591822
javaansehz <javaansehz@gmail.com>
2018-10-27 06:19:11 +0200

    contract-service

--- VertragService/src/main/java/de/vertragservice/repository/VertragRepository.java
+++
@@ -1,9 +0,0 @@
-package de.vertragservice.repository;
-
-import de.vertragservice.model.Vertrag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface VertragRepository extends JpaRepository<Vertrag, UUID> {
-}

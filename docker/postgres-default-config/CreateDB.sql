cf968dd01d04db428f3ec070bb99223e2179ffe9
javaansehz <javaansehz@gmail.com>
2018-10-27 09:31:17 +0200

    fix table name

--- docker/postgres-default-config/CreateDB.sql
+++ docker/postgres-default-config/CreateDB.sql
@@ -1,4 +1,4 @@
-CREATE TABLE partnerduplicates (
+CREATE TABLE clientduplicates (
   id SERIAL NOT NULL PRIMARY KEY,
   ts timestamp without time zone NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'CEST'),
   oldclientid TEXT,

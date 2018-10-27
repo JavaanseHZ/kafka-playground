16a80f8a1abcecaa06010a126842d40d737f9695
javaansehz <javaansehz@gmail.com>
2018-10-27 16:25:07 +0200

    fix internal docker connection dotnet

--- client-service/consumers/ConsumerConfiguration.cs
+++ client-service/consumers/ConsumerConfiguration.cs
@@ -1,10 +1,6 @@
-using client.models;
-using System.Threading;
 using System;
 using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace client.consumers {
     public class ConsumerConfiguration {
--- client-service/producers/ProducerConfiguration.cs
+++ client-service/producers/ProducerConfiguration.cs
@@ -1,3 +1,4 @@
+using System;
 using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
 
@@ -5,11 +6,13 @@ namespace client.producers {
     public class ProducerConfiguration {
 
         public  ProducerConfig producerConfig = new ProducerConfig {
-            BootstrapServers = "localhost:9092"
+             BootstrapServers = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("KAFKA_HOST")) ?
+                "localhost:9092" : Environment.GetEnvironmentVariable("KAFKA_HOST") + ":9092",
         };
 
         public AvroSerdeProviderConfig avroConfig = new AvroSerdeProviderConfig {
-            SchemaRegistryUrl = "http://localhost:8081",
+             SchemaRegistryUrl =  string.IsNullOrEmpty(Environment.GetEnvironmentVariable("KAFKA_HOST")) ?
+                "http://localhost:8081" : "http://" + Environment.GetEnvironmentVariable("KAFKA_HOST") + ":8081",
             SchemaRegistryRequestTimeoutMs = 5000,
             SchemaRegistryMaxCachedSchemas = 10,
             AvroSerializerBufferBytes = 50,

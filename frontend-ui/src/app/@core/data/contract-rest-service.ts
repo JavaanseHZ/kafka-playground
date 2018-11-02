e2d7a7f0320168b560255555c8deecde6f3e1815
javaansehz <javaansehz@gmail.com>
2018-11-02 05:18:29 +0100

    reveal presi

--- frontend-ui/src/app/@core/data/contract-rest-service.ts
+++ frontend-ui/src/app/@core/data/contract-rest-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 const endpoint = '/contract/api/contract';
 const httpOptions = {

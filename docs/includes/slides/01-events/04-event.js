5b6a4175ad176b8690fdab86cf9abc6f3d214343
javaansehz <javaansehz@gmail.com>
2018-11-03 19:13:32 +0100

    avro fix

--- docs/includes/slides/01-events/04-event.js
+++ docs/includes/slides/01-events/04-event.js
@@ -1,3 +1,6 @@
 var canvasEvent = document.getElementById('canvasEvent');
-var yCanvasEventCenter = (canvasEvent.height/2);
-rectangleArray(canvasEvent, 10, yCanvasEventCenter - 90, 700, 100, blue, [["Topic", burgundy], ["Key", "black"], ["Value", "black"]]);
+var ycanvasEventCenter = (canvasEvent.height/2);
+rectangle(canvasEvent, 10, ycanvasEventCenter - 60, 690, 120, blue, null, null);
+rectangle(canvasEvent, 30, ycanvasEventCenter - 40, 200, 80, burgundy, 'Topic', 'black');
+rectangle(canvasEvent, 250, ycanvasEventCenter - 40, 200, 80, yellow, 'Key', 'black');
+rectangle(canvasEvent, 470, ycanvasEventCenter - 40, 200, 80, yellow, 'Value', 'black');
--- docs/includes/slides/01-events/04-topic.js
+++ docs/includes/slides/01-events/04-topic.js
@@ -1,3 +1,6 @@
 var canvasTopic = document.getElementById('canvasTopic');
-var yCanvasTopicCenter = (canvasTopic.height/2);
-rectangleArray(canvasTopic, 10, yCanvasTopicCenter - 90, 700, 100, burgundy, [["Name", "black"], ["Key Type", "black"], ["Value Type", "black"]]);
+var ycanvasTopicCenter = (canvasTopic.height/2);
+rectangle(canvasTopic, 10, ycanvasTopicCenter - 60, 690, 120, burgundy, null, null);
+rectangle(canvasTopic, 30, ycanvasTopicCenter - 40, 200, 80, green, 'Name', 'black');
+rectangle(canvasTopic, 250, ycanvasTopicCenter - 40, 200, 80, yellow, 'Key Type', 'black');
+rectangle(canvasTopic, 470, ycanvasTopicCenter - 40, 200, 80, yellow, 'Value Type', 'black');

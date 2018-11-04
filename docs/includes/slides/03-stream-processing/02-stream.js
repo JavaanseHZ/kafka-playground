a6cca7721fa960a1a3383116c99f30dea9341be2
javaansehz <javaansehz@gmail.com>
2018-11-04 08:51:02 +0100

    consumer group offset base image

--- docs/includes/slides/03-stream-processing/02-stream.js
+++ docs/includes/slides/03-stream-processing/02-stream.js
@@ -1,7 +1,9 @@
 var canvasStream = document.getElementById('canvasStream');
 var ycanvasStreamCenter = (canvasStream.height/2);
-rectangle(canvasStream, 50, ycanvasStreamCenter - 50, 100, 100, blue, 'Prod', 'black');
-rectangleArray(canvasStream, 300, ycanvasStreamCenter - 50, 350, 100, burgundy, [['4', 'black'], ['3', 'black'], ['2', 'black'], ['1', 'black']]);
-rectangle(canvasStream, 800, ycanvasStreamCenter - 50, 100, 100, yellow, 'Cons', 'black');
-arrow (canvasStream, 170, ycanvasStreamCenter, 280, ycanvasStreamCenter, blue)
-arrow (canvasStream, 670, ycanvasStreamCenter, 780, ycanvasStreamCenter, yellow)
+var xcanvasStreamCenter = (canvasStream.width/2);
+rectangle(canvasStream, 10, ycanvasStreamCenter - 50, 100, 100, blue, 'Prod', 'black');
+rectangleArray(canvasStream, xcanvasStreamCenter - 175, ycanvasStreamCenter - 40, 350, 80, burgundy, [['4', 'black'], ['3', 'black'], ['2', 'black'], ['1', 'black']]);
+rectangle(canvasStream, canvasStream.width - 110, ycanvasStreamCenter - 50, 100, 100, yellow, 'Cons', 'black');
+
+arrow (canvasStream, 130, ycanvasStreamCenter, xcanvasStreamCenter - 195, ycanvasStreamCenter, blue)
+arrow (canvasStream, xcanvasStreamCenter + 195, ycanvasStreamCenter, canvasStream.width - 130, ycanvasStreamCenter, yellow)
--- docs/includes/slides/03-stream-processing/04-group-offset-01.js
+++ docs/includes/slides/03-stream-processing/04-group-offset-01.js
@@ -1,7 +1,36 @@
 var canvasGroupOffset01 = document.getElementById('canvasGroupOffset01');
 var ycanvasGroupOffset01Center = (canvasGroupOffset01.height/2);
-rectangle(canvasGroupOffset01, 50, ycanvasGroupOffset01Center - 50, 100, 100, blue, 'Prod', 'black');
-rectangleArray(canvasGroupOffset01, 300, ycanvasGroupOffset01Center - 50, 350, 100, burgundy, [['4', 'black'], ['3', 'black'], ['2', 'black'], ['1', 'black']]);
-rectangle(canvasGroupOffset01, 800, ycanvasGroupOffset01Center - 50, 100, 100, yellow, 'Cons', 'black');
-arrow (canvasGroupOffset01, 170, ycanvasGroupOffset01Center, 280, ycanvasGroupOffset01Center, blue)
-arrow (canvasGroupOffset01, 670, ycanvasGroupOffset01Center, 780, ycanvasGroupOffset01Center, yellow)
+var xcanvasGroupOffset01Center = (canvasGroupOffset01.width/2);
+
+rectangle(canvasGroupOffset01, xcanvasGroupOffset01Center - 175, ycanvasGroupOffset01Center - 220, 70, 40, burgundy, 'Part 1', 'black');
+rectangleArray(canvasGroupOffset01, xcanvasGroupOffset01Center - 175, ycanvasGroupOffset01Center - 180, 350, 80, burgundy, [['4', 'black'], ['3', 'black'], ['2', 'black'], ['1', 'black']]);
+rectangle(canvasGroupOffset01, xcanvasGroupOffset01Center - 175, ycanvasGroupOffset01Center + 180, 70, 40, burgundy, 'Part 2', 'black');
+rectangleArray(canvasGroupOffset01, xcanvasGroupOffset01Center - 175, ycanvasGroupOffset01Center + 100, 350, 80, burgundy, [['4', 'black'], ['3', 'black'], ['2', 'black'], ['1', 'black']]);
+
+rectangle(canvasGroupOffset01, 10, ycanvasGroupOffset01Center - 50, 100, 100, blue, 'Prod', 'black');
+arrow (canvasGroupOffset01, 130, ycanvasGroupOffset01Center - 20, 280, ycanvasGroupOffset01Center - 140, transientWhite)
+arrow (canvasGroupOffset01, 130, ycanvasGroupOffset01Center + 20, 280, ycanvasGroupOffset01Center + 140, transientWhite)
+
+
+//Group 1
+//rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 130, ycanvasGroupOffset01Center - 240, 120, 270, pink, null, null);
+
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 120, ycanvasGroupOffset01Center - 230, 100, 70, yellow, 'Cons 1', 'black');
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 120, ycanvasGroupOffset01Center - 160, 50, 40, yellow, '0', 'black');
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 70, ycanvasGroupOffset01Center - 160, 50, 40, yellow, '0', 'black');
+
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 120, ycanvasGroupOffset01Center - 90, 100, 70, yellow, 'Cons 2', 'black');
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 120, ycanvasGroupOffset01Center - 20, 50, 40, yellow, '0', 'black');
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 70, ycanvasGroupOffset01Center - 20, 50, 40, yellow, '0', 'black');
+
+arrow (canvasGroupOffset01, xcanvasGroupOffset01Center + 195, ycanvasGroupOffset01Center - 160, canvasGroupOffset01.width - 140, ycanvasGroupOffset01Center - 195, transientWhite)
+arrow (canvasGroupOffset01, xcanvasGroupOffset01Center + 195, ycanvasGroupOffset01Center + 120, canvasGroupOffset01.width - 140, ycanvasGroupOffset01Center - 55, transientWhite)
+
+// Group 2
+//rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 130, ycanvasGroupOffset01Center + 80, 120, 210, pink, null, null);
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 120, ycanvasGroupOffset01Center + 90, 100, 70, yellow, 'Cons 3', 'black');
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 120, ycanvasGroupOffset01Center + 160, 50, 40, yellow, '0', 'black');
+rectangle(canvasGroupOffset01, canvasGroupOffset01.width - 70, ycanvasGroupOffset01Center + 160, 50, 40, yellow, '0', 'black');
+
+arrow (canvasGroupOffset01, xcanvasGroupOffset01Center + 195, ycanvasGroupOffset01Center - 120, canvasGroupOffset01.width - 140, ycanvasGroupOffset01Center + 110, transientWhite)
+arrow (canvasGroupOffset01, xcanvasGroupOffset01Center + 195, ycanvasGroupOffset01Center + 160, canvasGroupOffset01.width - 140, ycanvasGroupOffset01Center + 140, transientWhite)
